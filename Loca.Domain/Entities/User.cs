3d8d44ce7067d658b4db052ffe2255e00290b8f7
Cheeky-Lord <lucasilva108@gmail.com>
2019-06-02 22:43:11 -0300

    It's just a test

--- Loca.Domain/Entities/User.cs
+++ Loca.Domain/Entities/User.cs
@@ -6,16 +6,27 @@ namespace Loca.Domain.Entities
 {
     public class User : Entity
     {
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         //public Name Name { get; private set; }
-        public DateTime BirthDate { get; private set; }
-        public string Email { get; private set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
         //public Email Email { get; private set; }
-        public string Password { get; private set; }
-        public string Image { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-        
+        public string Password { get; set; }
+        public string Image { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User()
+        {
+            FirstName = String.Empty;
+            LastName = String.Empty;
+            BirthDate = DateTime.Now;
+            Email = String.Empty;
+            Password = String.Empty;
+            Image = String.Empty;
+            CreatedAt = DateTime.Now;
+        }
+
         public User(string firstName, string lastName, string birthDate, string email, string password, string image)
         {
             FirstName = firstName;
--- Loca.Web/Controllers/UserController.cs
+++ Loca.Web/Controllers/UserController.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Loca.Domain.Entities;
 using Loca.Domain.Repositories;
-using Loca.Web.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Loca.Web.Controllers
